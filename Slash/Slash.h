5a38b19f64c0e95aa6a0f0b159b5cd6f51d183ff
Xinyi Zhuang <yingmu52@msn.com>
2018-02-12 22:00:50 -0800

    Initial Commit

---
+++ Slash/Slash.h
@@ -0,0 +1,19 @@
+//
+//  Slash.h
+//  Slash
+//
+//  Created by Xinyi Zhuang on 12/02/2018.
+//  Copyright © 2018 x52. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Slash.
+FOUNDATION_EXPORT double SlashVersionNumber;
+
+//! Project version string for Slash.
+FOUNDATION_EXPORT const unsigned char SlashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slash/PublicHeader.h>
+
+
