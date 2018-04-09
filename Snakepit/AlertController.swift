68e3bd6dae1d0d19b765128d3a7b8e556c1e72fe
Xinyi Zhuang <yingmu52@msn.com>
2018-04-09 00:30:04 -0700

    New AlertController

--- Snakepit/AlertController.swift
+++ Snakepit/AlertController.swift
@@ -1,6 +1,85 @@
 import Foundation
 import UIKit
 
+public extension UIAlertController {
+  public class func alert(_ name: String, message: String? = nil, acceptMessage: String = "OK", handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
+    let action = UIAlertAction(title: acceptMessage, style: .cancel, handler: handler)
+    return UIAlertController.alert(name, message: message, actions: [action])
+  }
+
+  public class func alert(_ name: String, message: String? = nil, actions: [UIAlertAction]) -> UIAlertController {
+    let alertController =  UIAlertController(title: name, message: message, preferredStyle: .alert)
+    actions.forEach(alertController.addAction)
+    return alertController
+  }
+
+  public class func alert(_ name: String, message: String? = nil, actionHandler: (() -> [UIAlertAction])) -> UIAlertController {
+      return UIAlertController.alert(name, message: message, actions: actionHandler())
+  }
+
+  public class func sheet( _ name: String, message: String? = nil, actions: [UIAlertAction]? = nil) -> UIAlertController {
+      let alertController =  UIAlertController(title: name, message: message, preferredStyle: .actionSheet)
+      actions?.forEach(alertController.addAction)
+      return alertController
+  }
+
+  public class func sheet( _ name: String, message: String? = nil, actionHandler: (() -> [UIAlertAction])) -> UIAlertController {
+      return UIAlertController.sheet(name, message: message, actions: actionHandler())
+  }
+
+  func action(title: String, style: UIAlertActionStyle = .default, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
+      addAction(title: title, style: style, handler: handler)
+      return self
+  }
+
+  func okable(handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
+    addOk(handler: handler)
+    return self
+  }
+
+  func cancelable(handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
+    addCancel(handler: handler)
+    return self
+  }
+
+  func addAction(title: String, style: UIAlertActionStyle = .default, handler: ((UIAlertAction) -> Void)? = nil) {
+    let action = UIAlertAction(title: title, style: style, handler: handler)
+    addAction(action)
+  }
+
+  func addOk(handler: ((UIAlertAction) -> Void)? = nil) {
+    addAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: handler)
+  }
+
+  func addCancel(handler: ((UIAlertAction) -> Void)? = nil) {
+    addAction(
+      title: NSLocalizedString("Cancel", comment: ""),
+      style: .cancel,
+      handler: handler
+    )
+  }
+}
+
+extension UIAlertAction {
+
+  convenience init(title: String?, handler: ((UIAlertAction) -> Void)? = nil) {
+    self.init(title: title, style: .default, handler: handler)
+  }
+
+  func appending(title: String, style: UIAlertActionStyle = .default,
+                 handler: ((UIAlertAction) -> Void)? = nil) -> [UIAlertAction] {
+    return [self, UIAlertAction(title: title, style: style, handler: handler)]
+  }
+}
+
+extension Collection where Iterator.Element == UIAlertAction {
+  func appending(title: String, style: UIAlertActionStyle = .default,
+                 handler: ((UIAlertAction) -> Void)? = nil) -> [UIAlertAction] {
+    return self + [UIAlertAction(title: title, style: style, handler: handler)]
+  }
+}
+
+
 extension UIViewController {
   @discardableResult public func showAlert(
     title: String,
